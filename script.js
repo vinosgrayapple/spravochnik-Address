d298f93e441c73f9c29e218fd752bc67c07349b6
Serhii Komarychev <komarichev@gmail.com>
2017-11-01 17:01:02 +0200

    commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let template = document.querySelector('#tmpl');
 let search = document.querySelector('#search');
-search.addEventListener('keyup', () => {
-    // console.log('hi');
-    str.includes('БЫТЬ')
-});
+
 users = [
     {
         name: "Вася",
@@ -21,7 +18,7 @@ users = [
         email: "kolya@gmail.com"
     },
     {
-        name: "Вася",
+        name: "коля",
         email: "kolya@gmail.com"
     },
     {
@@ -66,7 +63,11 @@ users = [
         email: "kolya@gmail.com"
     }
 ];
-
+search.addEventListener('keyup', () => {
+    // console.log('hi');
+    const inHTML = users.filter(user => user.name.includes(search.value)).reduce((a,b) => a + '<li>' + b.name + '<br>' + b.email + '</li>','');
+    template.innerHTML = inHTML;
+});
 const inHTML = users.reduce((a,b) => a + '<li>' + b.name + '<br>' + b.email + '</li>','');
 
 template.innerHTML = inHTML;
