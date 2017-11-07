ec4d9c34703c1e7f0f7f3f96e7e24d4e4e4cf880
Serhii Komarychev <komarichev@gmail.com>
2017-11-07 14:06:35 +0200

    add logic

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 let template = document.querySelector('#tmpl');
 let search = document.querySelector('#search');
 let units = document.querySelectorAll('.ui.compact.segment');
+let searchClear = document.querySelector('.close.link.icon');
+
+
+searchClear.addEventListener('click', () => {
+    search.value=""
+    printUsers(users);
+});
+
+const unitsClear = () => {
+    units.forEach(item => {
+        toggleClass(item, 'green', 'grey');
+       if ( item.querySelector('span.label') !== null) {
+        item.querySelector('span.label').remove();        
+       }
+            // item.querySelector('span.label').remove();
+            // console.log(item.querySelector('span'));
+            if (item.querySelector('span').innerText !== "Административные") {
+                item.style.paddingBottom = "14px";
+            } else {
+                item.style.paddingBottom = "11px";
+            }
+            // item.style.paddingBottom = "14px";
+    });
+};
+const printUsers = (arr) => {
+    template.innerHTML =  arr.reduce((a,b) =>  {
+            return a + 
+    `
+        <div class="card">
+            <div class="content">
+                <a class="ui image big label">
+                <img src=${b.img || "images/avatar/large/man.png"}>
+                ${b.name}
+                </a>
+                
+                <div class="meta">
+                    ${b.position || "&nbsp;"}
+                </div>
+                    <div class="description">
+                        <a href="mailto:${b.email}">${b.email}</a>
+                    </div>
+            </div>
+        </div>
+    `
+    },'');
+};
+const toggleClass = (context, delClass, addClass) => {
+    context.classList.remove(delClass);
+    context.classList.add(addClass)
+};
 users = [
     {
         name: "Олег Качур",
         email: "o.kachur@artwinery.com.ua",
         position: "Директор",
         unit: "Административные",
+        sex: "man",
         img: "images/avatar/large/kachur.jpg",
         birthday: "01.04.1983"
     },
@@ -15,12 +66,18 @@ users = [
         email: "igor.tolkachov@artwinery.com.ua",
         position: "Директор",
         unit: "Административные",
+        sex: "man",
         img: "images/avatar/large/steve.jpg",
         birthday: "15.01.1971"
     },
     {
-        name: "Вася",
-        email: "kolya@gmail.com"
+        name: "Виктория Малеваная",
+        email: "v.malovana@artwinery.com.ua",
+        position: "Директор",
+        unit: "Административные",
+        sex: "woman",
+        img: "",
+        birthday: ""
     },{
         name: "Вася",
         email: "kolya@gmail.com"
@@ -71,84 +128,57 @@ users = [
         email: "kolya@gmail.com"
     }
 ];
+units.forEach(unit => unit.onmousedown = unit.onselectstart = function() {
+    return false;
+});
 units.forEach(unit_item => unit_item.addEventListener('click',() => {
-    unit_item.classList.toggle('green');
-    unit_item.classList.toggle('grey');
-    let unit_ = unit_item.dataset.unit;
-    const inHTML = users.filter(user => user.unit.includes(unit_))
-        .reduce((a,b) =>  {
-            return a + 
-    `
-        <div class="card">
-            <div class="content">
-                <img class="right floated mini ui image" src=${b.img || "images/avatar/large/man.png"}>
-                    <div class="header">
-                         ${b.name}
-                    </div>
-                <div class="meta">
-                    ${b.position || "&nbsp;"}
-                </div>
-                    <div class="description">
-                        <a href="mailto:${b.email}">${b.email}</a>
-                    </div>
-            </div>
-        </div>
-    `
-    },'');
-    template.innerHTML = inHTML;
+    unitsClear();
+    toggleClass(unit_item, 'grey', 'green');
+    
+    search.blur();
+    let unit_ = unit_item.dataset.subunit;
+    
+    if (unit_item.querySelector('span').innerText !== "Административные") {
+        unit_item.style.paddingBottom = "0px";
+    } else {
+        unit_item.style.paddingBottom = "11px";
+    }
+
+    const span = document.createElement('span');
+    span.className = "floating ui purple label";
+    
+    
+    
+    if (unit_ === "Показать все") {
+        span.innerHTML = users.length;
+        printUsers(users);
+        
+        // toggleClass(unit_item, 'grey', 'green'); 
+    } else {
+        span.innerHTML = users.filter(user => user.unit ?  user.unit.includes(unit_) : false).length;
+         printUsers(users.filter(user => user.unit ?  user.unit.includes(unit_) : false));
+    }
+    unit_item.appendChild(span);
+   
 }));
 search.addEventListener('focus', () => {
+    unitsClear();
     units.forEach(unit=>{
-        unit.classList.remove('green')
-        unit.classList.add('grey');
+        toggleClass(unit,'green', 'grey');
     });
+    printUsers(users);
 });
 search.addEventListener('keyup', () => {
-     const inHTML = users.filter(user => 
+    printUsers(users.filter(user => 
     {
        const name = user.name.toLowerCase();
        const email = user.email.toLowerCase();
        const searchVal = search.value.toLowerCase(); 
        return name.includes(searchVal) || email.includes(searchVal);
-    }).reduce((a,b) =>  {
-        return a + 
-        `
-        <div class="card">
-        <div class="content">
-          <img class="right floated mini ui image" src=${b.img || "images/avatar/large/man.png"}>
-          <div class="header">
-          ${b.name}
-          </div>
-          <div class="meta">
-          ${b.position || "&nbsp;"}
-          </div>
-          <div class="description">
-          <a href="mailto:${b.email}">${b.email}</a>
-          </div>
-        </div>
-      </div>
-        `
-    },'');
-    template.innerHTML = inHTML;
+    }));
 });
-const inHTML = users.reduce((a,b) => {
-    return a + 
-    `
-    <div class="card">
-    <div class="content">
-      <img class="right floated mini ui image" src=${b.img || "images/avatar/large/man.png"}>
-      <div class="header">
-      ${b.name}
-      </div>
-      <div class="meta">
-      ${b.position || "&nbsp;"}
-      </div>   
-      <div class="description">
-      <a href="mailto:${b.email}">${b.email}</a>
-      </div>
-    </div>
-  </div>
-    `
-},'');
+printUsers(users);
 
-template.innerHTML = inHTML;
+// mt
+document.querySelector('.units :first-child').style.marginTop = "14px";
+document.querySelector('.units :first-child').style.paddingBottom = "11px";
